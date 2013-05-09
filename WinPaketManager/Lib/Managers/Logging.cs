d51deb7582d16e9378b58bcdc8a71ae51c9936f2
Fabian Berg <info@fabian-berg.de>
2013-05-09 12:53:28 +0200

    Ordered into new namespaces

--- WinPaketManager/Lib/Managers/Logging.cs
+++ WinPaketManager/Lib/Managers/Logging.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WinPaketManager.Lib
+namespace WinPaketManager.Lib.Managers
 {
     public class Logging
     {
--- WinPaketManager/Lib/Objects/Repository.cs
+++ WinPaketManager/Lib/Objects/Repository.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WinPaketManager.Lib.Exceptions;
 
-namespace WinPaketManager.Lib
+namespace WinPaketManager.Lib.Objects
 {
     public class Repository
     {
