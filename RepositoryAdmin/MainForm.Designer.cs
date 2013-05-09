bfc40b7f7101250706a2a4523306a54f1746c399
Marvin Karaschewski <marvin@kabesoft.de>
2013-05-09 21:47:42 +0200

    Added basic menustrip

---
+++ RepositoryAdmin/MainForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace RepositoryAdmin
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.FileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newRepositoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newPacketToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(808, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // FileToolStripMenuItem
+            // 
+            this.FileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newRepositoryToolStripMenuItem,
+            this.newPacketToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.FileToolStripMenuItem.Name = "FileToolStripMenuItem";
+            this.FileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.FileToolStripMenuItem.Text = "File";
+            // 
+            // newRepositoryToolStripMenuItem
+            // 
+            this.newRepositoryToolStripMenuItem.Name = "newRepositoryToolStripMenuItem";
+            this.newRepositoryToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.newRepositoryToolStripMenuItem.Text = "New Repository";
+            this.newRepositoryToolStripMenuItem.Click += new System.EventHandler(this.newRepositoryToolStripMenuItem_Click);
+            // 
+            // newPacketToolStripMenuItem
+            // 
+            this.newPacketToolStripMenuItem.Name = "newPacketToolStripMenuItem";
+            this.newPacketToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.newPacketToolStripMenuItem.Text = "New Packet";
+            this.newPacketToolStripMenuItem.Click += new System.EventHandler(this.newPacketToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(154, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 583);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem FileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newRepositoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newPacketToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ RepositoryAdmin/Packet.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace WinPaketManager.Lib.Objects
+{
+    public class Packet
+    {
+        public Packet(XmlNode node)
+        {
+            this.Name = node.Attributes["Name"].InnerText;
+            this.Version = node.Attributes["Version"].InnerText;
+            this.Folder = node.Attributes["Folder"].InnerText;
+            this.Category = node.Attributes["Category"].InnerText;
+            this.File32 = node.Attributes["File32"].InnerText;
+            this.File64 = node.Attributes["File64"].InnerText;
+            this.Image = node.Attributes["Image"].InnerText;
+        }
+
+        public string Name
+        {
+            get;
+            private set;
+        }
+
+        public string Version
+        {
+            get;
+            private set;
+        }
+
+        public string Folder
+        {
+            get;
+            private set;
+        }
+
+        public string Category
+        {
+            get;
+            private set;
+        }
+
+        public string File32
+        {
+            get;
+            private set;
+        }
+
+        public string File64
+        {
+            get;
+            private set;
+        }
+
+        public string Image
+        {
+            get;
+            private set;
+        }
+    }
+}
